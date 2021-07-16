bf34fb7ac78420538dedc277ecc76998df45462c
eliane-wang <48002094+eliane-wang@users.noreply.github.com>
2021-07-16 16:42:13 -0700

    Assignment | Heap

--- heap/main.cpp
+++ heap/main.cpp
@@ -1,114 +1,121 @@
+/* Creates a max heap from entered numbers or a file (containing a space-separated list of numbers). 
+   
+   Date: 07.16.2021
+   Author: Eliane Wang */
+
 #include <iostream>
 #include <cstring>
 #include <fstream>
-#include <string>
 
 using namespace std;
 
-void printArr(int* heap, int maxSize);
-void print(int* heap, int i, int level);
+void print(int* heap, int i, int level, int indexCount);
 void heapify(int* heap, int maxSize, int i);
 void heapSort(int* heap, int maxSize);
+void remove(int* heap, int indexCount);
+void deleteRoot(int* heap, int& indexCount);
 
 int main() {
-  int maxSize = 101;
+  int maxSize = 100; //heap stores up to 100 numbers
   int* heap = new int[maxSize];
-  for (int i = 0; i < maxSize; i++) {
+  for (int i = 0; i < maxSize; i++) { //heap values empty
     heap[i] = 0;
   }
-  int indexCount = 0;
-  char command[7];
-  bool running = true;
-
-  while (running == true) {
-    if (indexCount == maxSize) {
-      cout << "Heap full!" << endl;
-    }
-    cout << "Select an input method: 'MANUAL' or 'FILE'?" << endl;
-    cin >> command;
-    if (strcmp(command, "MANUAL") == 0) {//reads in numbers from console
-      cout << "How many numbers are you adding?" << endl;
-      int temp;
-      cin >> temp;
-      int counter = 0;
-      cout << "Input your numbers: " << endl;
-      while (counter != temp) {
-	cin >> heap[indexCount];
-	counter++;
+  int indexCount = 0; //size of heap
+  char command[7]; //commands
+  
+  cout << "Select an input method: 'MANUAL' or 'FILE'?" << endl;
+  cin >> command;
+  if (strcmp(command, "MANUAL") == 0) {//reads in numbers from console
+    cout << "How many numbers are you adding?" << endl;
+    int temp;
+    cin >> temp;
+    int counter = 0;
+    cout << "Input your numbers: " << endl;
+    while (counter != temp) {
+      cin >> heap[indexCount];
+      counter++;
+      indexCount++;
+    }    
+  }
+  if (strcmp(command, "FILE") == 0) { //reads in from file
+    cout << "enter a filename (ex. 'heap.txt'): " << endl;
+    char filename[10];
+    cin >> filename;
+    int heapdata;
+    ifstream myfile (filename);
+    if (myfile.is_open()) {
+      while (!myfile.eof()) { //not end of line
+	myfile >> heap[indexCount];
 	indexCount++;
       }
-      running = true;
+      myfile.close();
     }
-    if (strcmp(command, "FILE") == 0) { //reads in from file
-      cout << "enter a filename (ex. 'heap.txt'): " << endl;
-      char filename[10];
-      cin >> filename;
-      int heapdata;
-      ifstream myfile (filename);
-      if (myfile.is_open()) {
-	while (!myfile.eof()) { //not end of line
-	  myfile >> heap[indexCount];
-	  indexCount++;
-	}
-	myfile.close();
-      }
-      else {
-	cout << "unable to open file " << endl;
-      }
+    else { //invalid file name
+      cout << "unable to open file " << endl;
     }
-    cout << endl;
-    heapSort(heap,indexCount);
-    printArr(heap,maxSize);
-    cout << endl;
-    
-    print(heap,0,0);
-    
-    cout << endl;
   }
-}
-
-
-void printArr(int* heap, int maxSize) {
-  cout << endl << "Array: " << endl << endl;
-  for (int i = 0; i < maxSize; i++) {
-    //if (heap[i] != 0) { //within range
-      cout << heap[i] << " ";
-      //}
-  }
-}
-
-
-//prints tree 
-void print(int* heap, int i, int level) {
-  if (heap[i] != 0) {
-  level++;
-  print(heap,i*2+2,level); //right
   cout << endl;
-  for (int i = 1; i < level; i++) {
-    cout << "\t";
-  }
-  cout << heap[i];
-  print(heap,i*2+1,level); //left
+  heapSort(heap,indexCount); //sorts array to heap
+  cout << endl;
+  cout << "Displaying the heap: " << endl;  
+  print(heap,0,0,indexCount); //heap represented as tre
+  cout << endl << endl;
+  remove(heap,indexCount); //each element removed
+}
+
+//prints the heap in a tree  
+void print(int* heap, int i, int level, int indexCount) {
+  if (heap[i] != 0 && i < indexCount) { //element exists
+    level++;
+    print(heap,i*2+2,level, indexCount); //right child
+    cout << endl;
+    for (int i = 1; i < level; i++) {
+      cout << "\t";
+    }
+    cout << heap[i];
+    print(heap,i*2+1,level, indexCount); //left child
   }
 }
-void heapify(int* heap, int maxSize, int i /*root*/) { //https://www.geeksforgeeks.org/heap-sort/
+
+//rearranges nodes to maintain the max heap properties 
+void heapify(int* heap, int maxSize, int i) { //sourced from: geeksforgeeks.org/heap-sort/
   int largest = i;
   int l = 2*i+1;//left
   int r = 2*i+2;//right
-  if (l<maxSize && heap[l] > heap[largest]) {
+  if (l<maxSize && heap[l] > heap[largest]) { //if left child is largest
     largest = l;
   }
-  if (r < maxSize && heap[r] > heap[largest]) {
+  if (r < maxSize && heap[r] > heap[largest]) { //if right child is largest
     largest = r;
   }
-  if (largest != i) {
+  if (largest != i) { //rearranges if largest is not root
     swap(heap[i], heap[largest]);
     heapify(heap,maxSize,largest);
   }
 }
 
+//builds the max heap by rearranging the array
 void heapSort(int* heap, int maxSize) {
   for (int i = maxSize/2-1; i >= 0; i--) {
     heapify(heap,maxSize,i);
+  }
+}
+
+//deletes the root from the heap by replacing current root with last element
+void deleteRoot(int* heap, int& indexCount) {
+  int last = heap[indexCount-1];
+  cout << heap[0] << " "; //outputs
+  heap[0] = last; 
+  indexCount -= 1;
+  heapify(heap,indexCount,0);
+}
+
+//removes each element from the heap
+void remove(int* heap, int indexCount) {
+  cout << endl << "Removing from the heap: " << endl;
+  int n = indexCount; 
+  for (int i = 0; i < n; i++) {
+    deleteRoot(heap,indexCount); //deletes current root and updates heap
   }
 }
